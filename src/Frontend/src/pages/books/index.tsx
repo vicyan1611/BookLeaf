538dd695432b2fc9c9d7f85e5f11ad7895477ab9
Vinh <vinhpham123.np@gmail.com>
2024-11-29 21:22:46 +0700

    BOOK-26: Implement sidebar

--- src/Frontend/src/pages/books/index.tsx
+++ src/Frontend/src/pages/books/index.tsx
@@ -2,6 +2,8 @@ import bookLeafLogo from "../../assets/react.svg";
 import "./index.css";
 import { IoSearchOutline } from "react-icons/io5";
 import { LuBookOpen } from "react-icons/lu";
+import { FaCheck } from "react-icons/fa6";
+
 const SearchBar = () => {
   return (
     <div className="max-w-3xl w-full mx-auto">
@@ -21,7 +23,7 @@ const SearchBar = () => {
 
 const SideBar = () => {
   return (
-    <div className="w-64 border-r border-gray-200 p-4 bg-gray-50">
+    <div className="w-full border-r border-gray-200 p-4  shadow-md border-2 rounded-lg">
       {/* Recent Books Section */}
       <div className="mb-6">
         <div className="flex gap-2 text-gray-700 font-medium mb-3">
@@ -43,6 +45,37 @@ const SideBar = () => {
           </div>
         </div>
       </div>
+      {/* Categories Section */}
+      <div className="mb-6">
+        <div className="flex gap-2 text-gray-700 font-medium mb-3">
+          <LuBookOpen size={20} />
+          <span>Shelves</span>
+        </div>
+        <div className="ml-7 space-y-2">
+          <div className="text-left">Business</div>
+          <div className="text-left">Science</div>
+          <div className="text-left">Technology</div>
+        </div>
+      </div>
+      {/* Tags Section */}
+      <div className="mb-6">
+        <div className="flex gap-2 text-gray-700 font-medium mb-2">
+          <LuBookOpen size={20} />
+          <span>Tags</span>
+        </div>
+
+        <div className="ml-7 space-y-2">
+          <div className="text-left flex gap-2 items-center">
+            <FaCheck size={16} />
+            <span>tag1</span>
+          </div>
+          <div className="text-left">tag2</div>
+          <div className="text-left flex gap-2 items-center">
+            <FaCheck size={16} />
+            <span>tag3</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -50,7 +83,7 @@ const SideBar = () => {
 function Books() {
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-6">
         <img src={bookLeafLogo} alt="" />
         <div className="flex gap-3">
           <button className="font-semibold hover:bg-green-500 hover:text-white py-2 px-3 rounded">
@@ -68,9 +101,12 @@ function Books() {
         </div>
       </div>
       <SearchBar />
-      <div className="grid-cols-4">
-        <SideBar />
-        <div>
+      <div className=" grid grid-cols-4 mt-6 gap-1">
+        <div className=" col-start-1 col-span-1">
+          <SideBar />
+        </div>
+
+        <div className="bg-green-200 col-start-2 col-span-3">
           <h1>Books</h1>
         </div>
       </div>
