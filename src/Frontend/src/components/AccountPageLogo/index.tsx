e7c56493fd511cd3e117ea9a0e83d610f1ef3db7
ketamean <truongthanhtoan2003@gmail.com>
2024-12-08 03:26:00 +0700

    2 components for AccountPage: AccountPageLogo and AccountPageInput

---
+++ src/Frontend/src/components/AccountPageLogo/index.tsx
@@ -0,0 +1,11 @@
+import bookLeafLogo from '/assets/BookLeaf_Logo.svg'
+
+const AccountPageLogo = () => {
+    return (
+        <div className="container flex justify-center">
+            <img src={bookLeafLogo} alt="BookLeaf" />
+        </div>
+    )
+}
+
+export default AccountPageLogo
