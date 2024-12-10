6c554e8899f283eb03bfb73c2ed0342212ea2cbb
RokeMSE <rokeMSE123@gmail.com>
2024-12-10 09:14:29 +0700

    Changed Logo

--- src/Frontend/src/pages/userProfile/index.tsx
+++ src/Frontend/src/pages/userProfile/index.tsx
@@ -1,4 +1,4 @@
-import bookLeafLogo from "/assets/BookLeaf_Logo.svg"; // resize the logo to 20px
+import bookLeafLogo from "../../assets/BookLeaf_Logo_cropped.svg"; // resize the logo to 20px
 import "./index.css";
 import { useEffect, useState } from "react";
 import { FaCog, FaChartPie, FaBook } from "react-icons/fa";
