bd54110d4ee948054452b36e1a138cbed01f1320
ketamean <truongthanhtoan2003@gmail.com>
2024-12-11 15:17:54 +0700

    remove css for #root && modified login-related components

--- src/Frontend/src/components/AccountPageLogo/index.tsx
+++ src/Frontend/src/components/AccountPageLogo/index.tsx
@@ -2,7 +2,9 @@ import bookLeafLogo from '../../assets/BookLeaf_Logo_cropped.svg'
 const AccountPageLogo = () => {
     return (
         <div className="w-3/4 max-w-xs my-8 flex justify-center">
-            <img src={bookLeafLogo} alt="BookLeaf"/>
+            <a href="/">
+                <img src={bookLeafLogo} alt="BookLeaf"/>
+            </a>
         </div>
     )
 }
