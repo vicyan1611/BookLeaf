6674e3ca6bb668540b711c6498311791eb46d482
ketamean <truongthanhtoan2003@gmail.com>
2024-12-11 22:19:24 +0700

    Add updater to MetadataHandler

--- src/Backend/src/models/MetadataHandler/index.ts
+++ src/Backend/src/models/MetadataHandler/index.ts
@@ -1,9 +1,11 @@
 import { MetaAdder, adder as _adder } from "./MetaAdder";
 import { MetaFinder, finder as _finder } from "./MetaFinder";
+import { MetaUpdater, updater as _updater } from './MetaUpdater'
 
 class MetadataHandler {
     public adder: MetaAdder = _adder;
     public finder: MetaFinder = _finder;
+    public updater: MetaUpdater = _updater;
 }
 
 export const metaHandler = new MetadataHandler()
