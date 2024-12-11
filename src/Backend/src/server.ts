14f37175adbafe98563a47f86fd6453846a9e833
ketamean <truongthanhtoan2003@gmail.com>
2024-12-11 22:00:38 +0700

    setup database (NormalUsers) and login path for /login page

--- src/Backend/src/server.ts
+++ src/Backend/src/server.ts
@@ -4,13 +4,22 @@ import cors from "cors";
 import { connectMongoDB } from "./config/config";
 import { getAllBooks, getBookByID } from "./controllers/BookController";
 import morgan from "morgan";
+import { UserController } from './controllers/UserController'
+
 console.log(process.env.PORT)
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cors());
+const corsOptions: cors.CorsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:5173'], // Allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Allow cookies
+};
+
+app.use(cors(corsOptions));
 app.use(morgan("combined"));
 
 connectMongoDB();
@@ -30,13 +39,7 @@ app.get("/api/books/:bookID", getBookByID);
 // Simple books route
 app.get("/api/books", getAllBooks);
 
-app.post('/login', (req, res) => {
-  const username = req.body.username || null
-  const password = req.body.password || null
-  if (!username || !password) {
-    res.status(401).send('Invalid username or password')
-  }
-})
+app.post('/login', UserController.post);
 
 // Start server
 app.listen(port, () => {
