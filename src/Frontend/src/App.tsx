79fc5f3aa886e5ef010894e4f26e058b8fbf6410
Vinh <vinhpham123.np@gmail.com>
2024-12-11 22:47:11 +0700

    add responsive layout

--- src/Frontend/src/App.tsx
+++ src/Frontend/src/App.tsx
@@ -5,53 +5,55 @@ import Reader from "./pages/reading";
 import LoginPageNormalUser from "./pages/login";
 import LoginPageAdmin from "./pages/admin-login";
 import UserProfile from "./pages/userProfile";
-import { ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <LoginPageNormalUser />,
-	},
-	{
-		path: "/hello",
-		element: <HelloWorld />,
-	},
-	{
-		path: "/books",
-		element: <Books />,
-	},
-	{
-		path: "/books/:id",
-		element: <Reader />,
-	},
-	{
-		path: "/login",
-		element: <LoginPageNormalUser />,
-	},
-	{
-		path: "/admin-login",
-		element: <LoginPageAdmin />,
-	},
-	{
-		path: "/userProfile",
-		element: <UserProfile />,
-	},
+  {
+    path: "/",
+    element: <LoginPageNormalUser />,
+  },
+  {
+    path: "/hello",
+    element: <HelloWorld />,
+  },
+  {
+    path: "/books",
+    element: <Books />,
+  },
+  {
+    path: "/books/:id",
+    element: <Reader />,
+  },
+  {
+    path: "/login",
+    element: <LoginPageNormalUser />,
+  },
+  {
+    path: "/admin-login",
+    element: <LoginPageAdmin />,
+  },
+  {
+    path: "/userProfile",
+    element: <UserProfile />,
+  },
 ]);
 
 function App() {
-	return (<>
-    <ToastContainer 
-    position="top-center"
-    autoClose={3000}
-    hideProgressBar={false}
-    newestOnTop={true}
-    closeButton={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    />
-    <RouterProvider router={router} />
-  </>);
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeButton={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+      />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
