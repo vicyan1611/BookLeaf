cd37ded4688fcd77982d118a90f1bd99bb7590af
ketamean <truongthanhtoan2003@gmail.com>
2024-12-12 09:22:09 +0700

    set up database for Admin

--- src/Backend/src/scripts/setupDatabase.ts
+++ src/Backend/src/scripts/setupDatabase.ts
@@ -1,5 +1,7 @@
 import { setupBooks } from './setupBooks'
 import { setupNormalUsers } from './setupNormalUsers'
+import { setupAdmins } from './setupAdmins'
 
 setupBooks()
 setupNormalUsers()
+setupAdmins()
